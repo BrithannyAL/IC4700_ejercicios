09d7df070b63e47994a7430fb2ae7d3e321a59e2
Brithanny Arguello <49217000+BrithannyAL@users.noreply.github.com>
2023-09-09 13:02:08 -0600

    agregando ejercicios de F#

---
+++ Lenguaje_FSharp/EjerciciosExtraclase/ejercicios_f/ejercicios_f/Program.fs
@@ -0,0 +1,19 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open ejercicio1
+
+printfn "Primer ejercicio"
+let listaNumeros = [1;2;3;4;5]
+//Escribir izq o der
+let resultado1 = shift_fun 3 "izq" listaNumeros
+printfn "%A" resultado1
+
+printfn "\nSegundo ejercicio"
+let cadenas = ["Manzana"; "Banana"; "Cereza"; "Uva"; "Kiwi"]
+let resultado2 = filtarCadenas cadenas "na"
+printfn "%A" resultado2
+
+printfn "\nTercer ejercicio"
+let resultado3 = n_esimo 2 [1;2;3;4;5]
+printfn "%A" resultado3
+
+printfn "\nCuarto ejercicio"
---
+++ Lenguaje_FSharp/EjerciciosExtraclase/ejercicios_f/ejercicios_f/ejercicio1.fs
@@ -0,0 +1,26 @@
+﻿module ejercicio1
+
+//Para la creación de las lista mutables
+open System.Collections.Generic
+
+//La función recibe dos parámetros, una lista de string y un string
+let filtarCadenas (lista: string list) (subcadena: string) =
+    List.filter (fun (cadena : string)-> cadena.Contains(subcadena)) lista
+    //Filtra las cadenas que poseen la subcadena designada por el usuario
+
+//Función que recibe una lista de enterros y devuelve el entero que se encuentra en el indice
+let n_esimo (indice: int) (lista: int list) =
+    let resultado = List.mapi(fun indx num -> if indx = indice then Some num else None) lista
+    let elementos = List.choose id resultado
+    match elementos with
+    | [e] -> Some e
+    | _ -> None
+
+//Hace un shift (a la derecha o izquierda) en la lista la cantidad de pasos indicada por el usuario
+let shift_fun (n:int) (dir:string) (lista:int list) =
+    if dir = "der" then
+        let resultado = List.mapi(fun i num -> if i <= n-1 then 0 else lista.[i-n]) lista
+        resultado
+    else 
+        let resultado = List.mapi(fun i num -> if i > (lista.Length-n)-1 then 0 else lista.[i+n]) lista
+        resultado
